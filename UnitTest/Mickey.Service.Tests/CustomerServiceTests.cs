6b8a05c3813a3b92d45e4ae5126baa3ff4c81f33
MickeyShiue <e031304@gmail.com>
2018-12-20 22:31:53 +0800

    Add Test

---
+++ UnitTest/Mickey.Service.Tests/CustomerServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Mickey.DataAccess;
+using Mickey.Domain;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mickey.Service.Tests
+{
+    [TestFixture]
+    public class CustomerServiceTests
+    {
+        [Test]
+        public void Create_WhenCalled()
+        {
+            Customers model = new Customers { CompanyName = "MickeyCompany" };
+            IRepository<Customers> mockDAO = Substitute.For<IRepository<Customers>>(); //測試隔離框架
+
+            CustomersService service = new CustomersService(mockDAO);
+
+            service.Create(model);
+
+            mockDAO.Received(1).Create(model);
+        }
+    }
+}
